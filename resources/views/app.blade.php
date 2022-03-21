9ecd79cf651ad18dab5e9f3d27f42975629ca109
Nils Casties <nils.casties@gmail.com>
2022-03-21 13:29:35 +0100

    [OGT-test] Added types for css and js imports.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,13 +5,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>OGT-app</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link type="text/css" rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
 <body>
+<p>just a test on app-blade.php</p>
 <div id="app"></div>
 
-<script src="{{ asset('js/app.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
 <script>
     const configLeaflet = @json(config('leaflet'));
 </script>
