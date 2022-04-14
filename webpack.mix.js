e88e82cd545c2dd306dba6131b09eef9eb155cf6
Nils Casties <nils.casties@gmail.com>
2022-04-14 11:57:43 +0200

    [OGT-123] Excluded generating source maps for now.

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,8 +15,8 @@ mix.js('resources/js/app.js', 'public/js')
     .vue()
     .postCss('resources/css/app.css', 'public/css', [
         //
-    ])
-    .sourceMaps();
+    ]);
+    //.sourceMaps(); // excluded for now
 
 mix.js('./node_modules/leaflet/dist/leaflet.js', 'public/js')
     .postCss('./node_modules/leaflet/dist/leaflet.css', 'public/css', [
