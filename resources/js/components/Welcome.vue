bef3da37a80f94a5f2b3d4fecd0444dcd9790fee
Nils Casties <nils.casties@gmail.com>
2022-04-26 09:53:00 +0200

    [OGT-261] Added alternative welcome-page layout with buttons and covered background-image size.

--- resources/js/components/Welcome.vue
+++ resources/js/components/Welcome.vue
@@ -1,7 +1,7 @@
 <template>
     <v-container
         class="grey lighten-3 justify-center"
-        :class="{ background: showBackgroundImage }"
+        :class="{ background: showBackgroundImage, 'xs-bg-x-shifted': $vuetify.breakpoint.xs && (nextLayoutId == 0) }"
         fill-height
         fluid
         :style="{ backgroundImage: showBackgroundImage ? 'url(' + backgroundImageUrl + ')' : 'none' }"
@@ -31,6 +31,23 @@
                 </router-link>
             </v-col>
         </v-row>
+
+        <!-- for layout with buttons -->
+        <div class="mt-300" v-show="showMenuButtons">
+            <template v-for="menuButton in menuButtons">
+                <router-link :to="menuButton.routeTo">
+                    <v-btn
+                        class="mx-10 my-3"
+                        color="white"
+                        outlined
+                        rounded
+                        x-large
+                    >
+                        {{ menuButton.label }}
+                    </v-btn>
+                </router-link>
+            </template>
+        </div>
     </v-container>
 </template>
 
@@ -40,24 +57,56 @@ export default {
     data() {
         return {
             backgroundImageUrl: this.$ogtGlobals.proxyPath + '/images/de/background.png',
-            layoutLabels: ['A', 'B'],
-            nextLayoutId: 0,
+            layoutLabels: ['A', 'B', 'C'],
+            menuButtons: [
+                {
+                    label: 'Projekt',
+                    routeTo: '/project',
+                },
+                {
+                    label: 'Kartenansicht',
+                    routeTo: '/map',
+                },
+                {
+                    label: 'Datenvisualisierung',
+                    routeTo: '/charts',
+                },
+            ],
+            nextLayoutId: 2,
             showBanner: false,
             showBackgroundImage: true,
+            showMenuButtons: false,
         };
     },
     methods: {
         switchPageLayout() {
+            let backgroundSize = '100% auto';
+
             if (this.nextLayoutId == 0) {
                 this.showBanner = true;
                 this.showBackgroundImage = false;
+                this.showMenuButtons = false;
                 this.nextLayoutId = 1;
             }
             else if (this.nextLayoutId == 1) {
                 this.showBanner = false;
                 this.showBackgroundImage = true;
+                this.showMenuButtons = false;
+                this.nextLayoutId = 2;
+            }
+            else if (this.nextLayoutId == 2) {
+                this.showBanner = false;
+                this.showBackgroundImage = true;
+                this.showMenuButtons = true;
+                backgroundSize = 'cover';
                 this.nextLayoutId = 0;
             }
+
+            const backgrounds = document.querySelectorAll('.background');
+
+            backgrounds.forEach(background => {
+                background.style.backgroundSize = backgroundSize;
+            });
         },
     },
 };
@@ -70,8 +119,17 @@ export default {
     background-size: 100% auto;
 }
 
+.mt-300 {
+    margin-top: 300px;
+}
+
 /* to lowercase text within Vuetify buttons */
 .noneTextTransform {
     text-transform: none;
+}
+
+/* used move center of background image for extra small window size (< 600px) */
+.xs-bg-x-shifted {
+    background-position-x: -650px;
 }
 </style>
