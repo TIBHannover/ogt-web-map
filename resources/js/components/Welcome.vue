1893ffca578d7490e2ce811fa3800dc472b2e5e7
Nils Casties <nils.casties@gmail.com>
2022-04-27 08:53:14 +0200

    [OGT-261] Added alternative layout for welcome-page where header text is not part of background image.

--- resources/js/components/Welcome.vue
+++ resources/js/components/Welcome.vue
@@ -1,7 +1,7 @@
 <template>
     <v-container
         class="grey lighten-3 justify-center"
-        :class="{ background: showBackgroundImage, 'xs-bg-x-shifted': $vuetify.breakpoint.xs && (selectedLayoutId == 2) }"
+        :class="{ background: showBackgroundImage, 'xs-bg-x-shifted': $vuetify.breakpoint.xs && ([2, 4].includes(selectedLayoutId)) }"
         fill-height
         fluid
         :style="{ backgroundImage: showBackgroundImage ? 'url(' + backgroundImageUrl + ')' : 'none' }"
@@ -33,7 +33,16 @@
         </v-row>
 
         <!-- for layout with buttons -->
-        <div class="mt-300" v-show="showMenuButtons">
+        <div
+            :class="{'mt-300': ([2, 3].includes(selectedLayoutId))}"
+            v-show="showMenuButtons"
+        >
+            <p class="text-h1 white--text" v-show="showHeaderText">
+                <span class="text-h4 white--text text-sm-h1">Gestapo.Terror.Orte</span>
+                <br>
+                <span class="text-h5 white--text text-sm-h3 ml-1">in Niedersachsen 1933 – 1945</span>
+                <br>
+            </p>
             <template v-for="menuButton in menuButtons">
                 <router-link :to="menuButton.routeTo">
                     <v-btn
@@ -61,7 +70,8 @@ export default {
             // B: background image, stretched to width (100%) => grey background at top and bottom on small displays
             // C: background image, cover, menu buttons => background image text lost on small devices
             // D: background image, stretched to height & width (100%), show menu buttons delayed => background image looks distorted
-            layoutLabels: ['A', 'B', 'C', 'D'],
+            // E: background image, cover, menu buttons, title as text => style (e.g. font type, spacing) must be adjusted
+            layoutLabels: ['A', 'B', 'C', 'D', 'E'],
             menuButtons: [
                 {
                     label: 'Projekt',
@@ -80,37 +90,55 @@ export default {
             selectedLayoutId: 1,
             showBackgroundImage: true,
             showBanner: false,
+            showHeaderText: false,
             showMenuButtons: false,
         };
+    },
+    mounted() {
+        this.selectedLayoutId -= 1;
+        this.switchPageLayout();
     },
     methods: {
         switchPageLayout() {
             clearTimeout(this.menuButtonsShowTimeoutId);
             let backgroundSize = '100% auto';
             this.selectedLayoutId = (this.selectedLayoutId + 1) % this.layoutLabels.length;
+            this.backgroundImageUrl = this.$ogtGlobals.proxyPath + '/images/de/backgroundWithText.png';
 
             if (this.selectedLayoutId == 0) {
                 this.showBackgroundImage = false;
                 this.showBanner = true;
+                this.showHeaderText = false;
                 this.showMenuButtons = false;
             }
             else if (this.selectedLayoutId == 1) {
                 this.showBackgroundImage = true;
                 this.showBanner = false;
+                this.showHeaderText = false;
                 this.showMenuButtons = false;
             }
             else if (this.selectedLayoutId == 2) {
                 this.showBackgroundImage = true;
                 this.showBanner = false;
+                this.showHeaderText = false;
                 this.showMenuButtons = true;
                 backgroundSize = 'cover';
             }
             else if (this.selectedLayoutId == 3) {
                 this.showBackgroundImage = true;
                 this.showBanner = false;
+                this.showHeaderText = false;
                 this.showMenuButtons = false;
                 backgroundSize = '100% 100%';
                 this.setMenuButtonsShowTimeout();
+            }
+            else if (this.selectedLayoutId == 4) {
+                this.backgroundImageUrl = this.$ogtGlobals.proxyPath + '/images/backgroundWithoutText.jpg';
+                this.showBackgroundImage = true;
+                this.showBanner = false;
+                this.showHeaderText = true;
+                this.showMenuButtons = true;
+                backgroundSize = 'cover';
             }
 
             const backgrounds = document.querySelectorAll('.background');
