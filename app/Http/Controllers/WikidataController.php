c45ea7942120a418197d248adaea204c8e598445
Nils Casties <nils.casties@gmail.com>
2022-08-14 11:31:37 +0200

    [OGT-292] Validate location response data header.

--- app/Http/Controllers/WikidataController.php
+++ app/Http/Controllers/WikidataController.php
@@ -6,6 +6,7 @@ use App\Http\Clients\WikidataClient;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class WikidataController extends Controller
 {
@@ -19,10 +20,29 @@ class WikidataController extends Controller
     public function getPlaces(WikidataClient $wikidataClient) : Response
     {
         $placesResponse = $wikidataClient->queryPlaces();
-
         $validator = Validator::make($placesResponse, [
             'head'             => 'required|array:vars|size:1',
-            'head.vars'        => 'required|array|size:12',
+            'head.vars'        => [
+                'required',
+                'array',
+                'size:12',
+                Rule::in(
+                    [
+                        'item',
+                        'itemLabel',
+                        'itemDescription',
+                        'property',
+                        'statement',
+                        'propertyValue',
+                        'propertyValueLabel',
+                        'propertyTimePrecision',
+                        'qualifier',
+                        'qualifierValue',
+                        'qualifierValueLabel',
+                        'qualifierTimePrecision',
+                    ]
+                ),
+            ],
             'results'          => 'required|array:bindings|size:1',
             'results.bindings' => 'required|array',
         ]);
