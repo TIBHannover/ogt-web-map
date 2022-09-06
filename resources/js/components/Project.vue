0ea09c354afb78ae26089b0227791bb02687cc01
Nils Casties <nils.casties@gmail.com>
2022-09-06 19:58:43 +0200

    [OGT-303] Added infos about collaboration, funding and duration of project to project page.

--- resources/js/components/Project.vue
+++ resources/js/components/Project.vue
@@ -12,7 +12,7 @@
 
         <v-row>
             <!-- table of contents sidebar -->
-            <v-col class="col-12 col-sm-2 order-sm-last" style="min-width: 215px">
+            <v-col class="col-12 col-sm-2 order-sm-last" style="min-width: 300px">
                 <section class="sticky-top pt-3">
                     <h6 class="text-h6">Inhaltsübersicht</h6>
                     <div class="toc-border-left">
@@ -55,15 +55,78 @@
                                 </v-list-item-subtitle>
                             </v-list-item-content>
                         </v-list-item>
-                    </div>
-                    <v-divider></v-divider>
+                        <v-divider></v-divider>
 
+                        <v-list-item three-line>
+                            <v-list-item-content>
+                                <v-list-item-title class="mb-4">
+                                    Kooperationsprojekt
+                                </v-list-item-title>
+                                <v-list-item-subtitle class="mb-2">
+                                    <a href="https://www.tib.eu/de/" target="_blank">
+                                        <v-img alt="Logo der Technische Informationsbibliothek (TIB)"
+                                               contain
+                                               :src="this.$ogtGlobals.proxyPath + '/images/logo-tib.png'"
+                                               style="max-width: 270px"
+                                        ></v-img>
+                                    </a>
+                                </v-list-item-subtitle>
+                                <v-list-item-subtitle>
+                                    <a href="https://www.stiftung-ng.de/" target="_blank">
+                                        <v-img alt="Logo der Stiftung niedersächsische Gedenkstätten"
+                                               contain
+                                               :src="this.$ogtGlobals.proxyPath + '/images/logo-sng.svg'"
+                                               style="max-width: 270px"
+                                        ></v-img>
+                                    </a>
+                                </v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+                        <v-divider></v-divider>
+
+                        <v-list-item three-line>
+                            <v-list-item-content>
+                                <v-list-item-title class="mb-4">
+                                    Förderung
+                                </v-list-item-title>
+                                <v-list-item-subtitle class="mb-2">
+                                    <a href="https://www.mwk.niedersachsen.de/startseite/" target="_blank">
+                                        <v-img
+                                            alt="Logo der Förderung durch das Niedersächsisches Ministerium für Wissenschaft und Kultur"
+                                            contain
+                                            :src="this.$ogtGlobals.proxyPath + '/images/logo-mwk-funding.png'"
+                                            style="max-width: 270px"
+                                        ></v-img>
+                                    </a>
+                                </v-list-item-subtitle>
+                                <v-list-item-subtitle>
+                                    <a class="hyphens-auto white-space-normal"
+                                       href="https://www.mwk.niedersachsen.de/startseite/aktuelles/presseinformationen/land-fordert-geistes-kultur-und-sozialwissenschaften-mit-4-5-mio-euro-191014.html"
+                                       lang="de"
+                                       target="_blank">Förderprogramm PRO*Niedersachsen</a>
+                                </v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+                        <v-divider></v-divider>
+
+                        <v-list-item two-line>
+                            <v-list-item-content>
+                                <v-list-item-title class="mb-4">
+                                    Laufzeit
+                                </v-list-item-title>
+                                <v-list-item-subtitle>
+                                    11/2020 – 01/2023
+                                </v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+                        <v-divider></v-divider>
+                    </div>
                 </section>
             </v-col>
 
             <!-- scrollable content -->
             <v-col>
-                <section id="researchObjectives" class="mb-16">
+                <section id="researchObjectives" class="mt-3 mb-16">
                     <h1 class="text-h5 text-md-h4 mb-5">
                         Forschungsfragen & Projektziele
                     </h1>
