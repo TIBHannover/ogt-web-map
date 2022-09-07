f258cf3ee5fac8e2991f2b6c818d0f8790f338aa
Nils Casties <nils.casties@gmail.com>
2022-09-07 09:33:11 +0200

    [OGT-303] Added infos about team and partners of project to project page.

--- resources/js/components/Project.vue
+++ resources/js/components/Project.vue
@@ -125,22 +125,117 @@
             </v-col>
 
             <!-- scrollable content -->
-            <v-col>
+            <v-col class="hyphens-auto" lang="de">
                 <section id="researchObjectives" class="mt-3 mb-16">
                     <h1 class="text-h5 text-md-h4 mb-5">
                         Forschungsfragen & Projektziele
                     </h1>
                 </section>
+
                 <section id="teamAndPartners" class="mb-16">
                     <h1 class="text-h5 text-md-h4 mb-5">
                         Team & Partner:innen
                     </h1>
+                    <h2 class="text-h6 text-md-h5 mb-3">
+                        Einleitung
+                    </h2>
+                    <p>
+                        Das Projekt „Orte des Gestapoterrors im heutigen Niedersachsen“ (OGT) ist ein
+                        Kooperationsprojekt zwischen der Stiftung niedersächsische Gedenkstätten (SnG), zu deren Auftrag
+                        die Forschungs- und Bildungsarbeit über die nationalsozialistische Verfolgungspolitik zählt, und
+                        der Technischen Informationsbibliothek (TIB), die auf die digitale Erschließung und Aufbereitung
+                        von Daten und deren freie Zugänglichkeit spezialisiert ist.
+                    </p>
+                    <h2 class="text-h6 text-md-h5 mb-3">
+                        Team
+                    </h2>
+                    <h3 class="text-subtitle-1 text-md-h6 mb-1">
+                        Technische Informationsbibliothek (TIB) – Open Science Lab (OSL)
+                    </h3>
+                    <ul class="mb-4">
+                        <li>
+                            <a href="https://vivo.tib.eu/fis/individual/n0000-0003-0232-7085" target="_blank">
+                                Lambert Heller
+                            </a>
+                            &nbsp;– Leitung Open Science Lab der TIB
+                        </li>
+                        <li>
+                            <a href="https://vivo.tib.eu/fis/individual/n0000-0002-3075-7640" target="_blank">
+                                Prof. Dr. Ina Blümel
+                            </a>
+                            &nbsp;– Projektleitung
+                        </li>
+                        <li>Lisa Groh-Trautmann (ab 02/2022) – Projektkoordination & Recherche</li>
+                        <li>Dr. Janine Doerry (bis 10/2021) – Projektkoordination & Recherche</li>
+                        <li>Nils Casties – Webentwicklung & Projektkoordination</li>
+                        <li>
+                            <a href="https://vivo.tib.eu/fis/individual/n0000-0002-2593-8754" target="_blank">
+                                Lucia Sohmen
+                            </a>
+                            &nbsp;– Wissenschaftliche Mitarbeiterin
+                        </li>
+                        <li>
+                            <a href="https://nadinekloever.com/" target="_blank">
+                                Nadine Klöver
+                            </a>
+                            &nbsp;– Grafik-Design | Advertising-Design
+                        </li>
+                        <li>Tobias Mielich – Studentischer Mitarbeiter, Fachmaster Geschichte (Master of Arts)</li>
+                    </ul>
+
+                    <h2 class="text-h6 text-md-h5 mb-3">
+                        Partner
+                    </h2>
+                    <h3 class="text-subtitle-1 text-md-h6 mb-1">
+                        Stiftung niedersächsische Gedenkstätten (SnG)
+                    </h3>
+                    <p>
+                        <a href="https://www.stiftung-ng.de/" target="_blank">
+                            <v-img
+                                alt="Logo der Stiftung niedersächsische Gedenkstätten"
+                                contain
+                                max-height="150px"
+                                max-width="200px"
+                                :src="this.$ogtGlobals.proxyPath + '/images/logo-sng.svg'"
+                            ></v-img>
+                        </a>
+                    </p>
+
+                    <h3 class="text-subtitle-1 text-md-h6 mb-1">
+                        Gedenkstätten Gestapokeller und Augustaschacht
+                    </h3>
+                    <div class="pb-16">
+                        <div class="float-left">
+                            <a href="https://gedenkstaetten-augustaschacht-osnabrueck.de/augustaschacht"
+                               target="_blank"
+                            >
+                                <v-img
+                                    alt="Logo der Gedenkstätte Augustaschacht"
+                                    height="60px"
+                                    :src="this.$ogtGlobals.proxyPath + '/images/logo-augustaschacht.svg'"
+                                    width="175px"
+                                ></v-img>
+                            </a>
+                        </div>
+                        <div class="float-left">
+                            <a href="https://gedenkstaetten-augustaschacht-osnabrueck.de/gestapokeller" target="_blank">
+                                <v-img
+                                    alt="Logo der Gedenkstätte Gestapokeller"
+                                    height="60px"
+                                    :src="this.$ogtGlobals.proxyPath + '/images/logo-gestapokeller.svg'"
+                                    width="175px"
+                                ></v-img>
+                            </a>
+                        </div>
+                    </div>
                 </section>
+
                 <section id="presentationsAndPublications" class="mb-16">
                     <h1 class="text-h5 text-md-h4 mb-5">
                         Projektpräsentationen & Publikationen
                     </h1>
                 </section>
+
                 <section id="pressReports" class="mb-16">
                     <h1 class="text-h5 text-md-h4 mb-5">
                         Presseberichte
