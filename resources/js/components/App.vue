8bacf7a28e536c2f5b25de0cbe73686c2eac56a2
Nils Casties <nils.casties@gmail.com>
2022-09-08 15:17:52 +0200

    Added feedback email in footer.

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -8,7 +8,9 @@
         <v-footer fixed padless width="100%" class="grey">
             <v-card class="flex grey" flat>
                 <v-card-text class="pa-0 text-subtitle-2 font-weight-bold text--darken-4 text-center">
-                    DIESE WEBSITE IST NOCH IM AUFBAU /<br>THIS WEBSITE IS STILL UNDER CONSTRUCTION
+                    DIESE WEBSITE IST NOCH IM AUFBAU /<br>
+                    THIS WEBSITE IS STILL UNDER CONSTRUCTION<br>
+                    Feedback und Anregungen an: <a @click.stop="setMailToLink('mjtb/hspi')">E-Mail</a>
                 </v-card-text>
             </v-card>
         </v-footer>
@@ -21,6 +23,34 @@ import NavigationSidebar from './navigation/NavigationSidebar';
 export default {
     name: 'App',
     components: {NavigationSidebar},
+    methods: {
+        /**
+         * Set decrypted email link to open with default mail program.
+         *
+         * @param encryptedEmail
+         */
+        setMailToLink: function (encryptedEmail) {
+            window.location.href = 'mailto:' + this.decryptEmail(encryptedEmail + 'Aujc/fv');
+        },
+        /**
+         * Decrypt email - https://www.math.uni-hamburg.de/it/dienste/encryptma.html
+         *
+         * @param encryptedEmail
+         * @returns {string} decrypted email
+         */
+        decryptEmail: function (encryptedEmail) {
+            let utf16CharCode = 0;
+            let decryptedEmail = '';
+            for (let i = 0; i < encryptedEmail.length; i++) {
+                utf16CharCode = encryptedEmail.charCodeAt(i);
+                if (utf16CharCode >= 8364) {
+                    utf16CharCode = 128;
+                }
+                decryptedEmail += String.fromCharCode(utf16CharCode - (1));
+            }
+            return decryptedEmail;
+        },
+    },
 };
 </script>
 
