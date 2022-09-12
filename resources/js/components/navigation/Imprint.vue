aac5f921c71e23b962bc8efad6c5e23c3b38f1f3
Nils Casties <nils.casties@gmail.com>
2022-09-12 15:04:01 +0200

    [OGT-308] Added imprint.

--- resources/js/components/navigation/Imprint.vue
+++ resources/js/components/navigation/Imprint.vue
@@ -1,26 +1,118 @@
 <template>
-    <v-list rounded>
-        <v-subheader class="text-uppercase">Impressum</v-subheader>
-        <v-list-item>
-            <v-list-item-content class="text-justify">
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-                invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-                et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-                Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-                voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
-                gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-            </v-list-item-content>
-        </v-list-item>
-    </v-list>
+    <section>
+        <v-list>
+            <v-subheader class="text-h5">Impressum</v-subheader>
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-subtitle class="hyphens-auto white-space-normal" lang="de">
+                        Impressum zu dieser Website – zugleich Anbieterkennzeichnung gem. § 5 Telemediengesetz (TMG)
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title>Herausgeber:</v-list-item-title>
+                    <v-list-item-subtitle>
+                        Technische Informationsbibliothek (TIB)
+                        <br>
+                        Welfengarten 1 B, 30167 Hannover
+                        <br>
+                        Postfach 6080, 30060 Hannover
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title>Vertretungsberechtigte Person:</v-list-item-title>
+                    <v-list-item-subtitle>
+                        Prof. Dr. Sören Auer (Direktor der TIB)
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-subtitle class="hyphens-auto white-space-normal" lang="de">
+                        Die Technische Informationsbibliothek (TIB) ist eine Stiftung öffentlichen Rechts des Landes
+                        Niedersachsen.
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title>Aufsichtsbehörde:</v-list-item-title>
+                    <v-list-item-subtitle class="hyphens-auto white-space-normal" lang="de">
+                        Ministerium für Wissenschaft und Kultur des Landes Niedersachsen
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title>Kontakt:</v-list-item-title>
+                    <v-list-item-subtitle>
+                        Telefon: 0511 762-8989
+                        <br>
+                        Fax: 0511 762-4076
+                        <br>
+                        E-Mail:
+                        <email encryptedEmail="jogpsnbujpo" :showEmail=true></email>
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title>Umsatzsteuer-Identifikationsnummer:</v-list-item-title>
+                    <v-list-item-subtitle>
+                        DE 214931803
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title>Redaktion:</v-list-item-title>
+                    <v-list-item-subtitle class="hyphens-auto white-space-normal" lang="de">
+                        Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV: Dr. Sandra Niemeyer; E-Mail:<br>
+                        <email encryptedEmail="tboesb/ojfnfzfs" :showEmail=true></email>
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title>Urheberrechtshinweis:</v-list-item-title>
+                    <v-list-item-subtitle class="hyphens-auto white-space-normal" lang="de">
+                        Das Layout der Homepage, die verwendeten Grafiken sowie die sonstigen Inhalte sind
+                        urheberrechtlich geschützt.
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+        </v-list>
+    </section>
 </template>
 
 <script>
+import Email from '../helpers/Email';
+
 export default {
     name: 'Imprint',
+    components: {Email},
 };
 </script>
 
 <style scoped>
+.hyphens-auto {
+    hyphens: auto;
+}
 
+/* to enable linebreaks for long labels */
+.white-space-normal {
+    white-space: normal;
+}
 </style>
