0127796720d2d1b34d1b4472f783f4484d3f2ede
Nils Casties <nils.casties@gmail.com>
2022-09-17 20:52:42 +0200

    [OGT-309] Refactored table of contents as embeddable template component.

--- resources/js/components/Project.vue
+++ resources/js/components/Project.vue
@@ -14,125 +14,8 @@
             <!-- space reserved for opened navigation menu -->
             <v-col class="hidden-sm-and-down" style="min-width: 375px; max-width: 375px"></v-col>
 
-            <!-- table of contents sidebar -->
             <v-col class="col-12 col-sm-2 order-sm-last" style="min-width: 300px">
-                <section class="sticky-top pt-3">
-                    <h6 class="text-h6">Inhaltsübersicht</h6>
-                    <div class="toc-border-left">
-                        <v-list-item>
-                            <v-list-item-content>
-                                <v-list-item-subtitle>
-                                    <a class="hyphens-auto white-space-normal" href="#researchObjectives" lang="de">
-                                        Forschungsfragen & Projektziele
-                                    </a>
-                                </v-list-item-subtitle>
-                            </v-list-item-content>
-                        </v-list-item>
-                        <v-list-item>
-                            <v-list-item-content>
-                                <v-list-item-subtitle>
-                                    <a class="hyphens-auto white-space-normal" href="#teamAndPartners" lang="de">
-                                        Team & Partner:innen
-                                    </a>
-                                </v-list-item-subtitle>
-                            </v-list-item-content>
-                        </v-list-item>
-                        <v-list-item>
-                            <v-list-item-content>
-                                <v-list-item-subtitle>
-                                    <a
-                                        class="hyphens-auto white-space-normal"
-                                        href="#presentationsAndPublications"
-                                        lang="de"
-                                    >
-                                        Projektpräsentationen & Publikationen
-                                    </a>
-                                </v-list-item-subtitle>
-                            </v-list-item-content>
-                        </v-list-item>
-                        <v-list-item>
-                            <v-list-item-content>
-                                <v-list-item-subtitle>
-                                    <a class="hyphens-auto white-space-normal" href="#pressReports" lang="de">
-                                        Presseberichte
-                                    </a>
-                                </v-list-item-subtitle>
-                            </v-list-item-content>
-                        </v-list-item>
-                        <v-divider></v-divider>
-
-                        <v-list-item three-line>
-                            <v-list-item-content>
-                                <v-list-item-title class="mb-4">
-                                    Kooperationsprojekt
-                                </v-list-item-title>
-                                <v-list-item-subtitle class="mb-2">
-                                    <a href="https://www.tib.eu/de/" target="_blank">
-                                        <v-img
-                                            alt="Logo der Technische Informationsbibliothek (TIB)"
-                                            contain
-                                            max-width="200px"
-                                            :src="this.$ogtGlobals.proxyPath + '/images/logo-tib.png'"
-                                        ></v-img>
-                                    </a>
-                                </v-list-item-subtitle>
-                                <v-list-item-subtitle>
-                                    <a href="https://www.stiftung-ng.de/" target="_blank">
-                                        <v-img
-                                            alt="Logo der Stiftung niedersächsische Gedenkstätten"
-                                            contain
-                                            max-height="150px"
-                                            max-width="200px"
-                                            :src="this.$ogtGlobals.proxyPath + '/images/logo-sng.svg'"
-                                        ></v-img>
-                                    </a>
-                                </v-list-item-subtitle>
-                            </v-list-item-content>
-                        </v-list-item>
-                        <v-divider></v-divider>
-
-                        <v-list-item three-line>
-                            <v-list-item-content>
-                                <v-list-item-title class="mb-4">
-                                    Förderung
-                                </v-list-item-title>
-                                <v-list-item-subtitle class="mb-2">
-                                    <a href="https://www.mwk.niedersachsen.de/startseite/" target="_blank">
-                                        <v-img
-                                            alt="Logo der Förderung durch das Niedersächsisches Ministerium für Wissenschaft und Kultur"
-                                            contain
-                                            max-width="200px"
-                                            :src="this.$ogtGlobals.proxyPath + '/images/logo-mwk-funding.png'"
-                                        ></v-img>
-                                    </a>
-                                </v-list-item-subtitle>
-                                <v-list-item-subtitle>
-                                    <a
-                                        class="hyphens-auto white-space-normal"
-                                        href="https://www.mwk.niedersachsen.de/startseite/aktuelles/presseinformationen/land-fordert-geistes-kultur-und-sozialwissenschaften-mit-4-5-mio-euro-191014.html"
-                                        lang="de"
-                                        target="_blank"
-                                    >
-                                        Förderprogramm PRO*Niedersachsen
-                                    </a>
-                                </v-list-item-subtitle>
-                            </v-list-item-content>
-                        </v-list-item>
-                        <v-divider></v-divider>
-
-                        <v-list-item two-line>
-                            <v-list-item-content>
-                                <v-list-item-title class="mb-4">
-                                    Laufzeit
-                                </v-list-item-title>
-                                <v-list-item-subtitle>
-                                    11/2020 – 01/2023
-                                </v-list-item-subtitle>
-                            </v-list-item-content>
-                        </v-list-item>
-                        <v-divider></v-divider>
-                    </div>
-                </section>
+                <table-of-contents-sidebar :headings="headings"></table-of-contents-sidebar>
             </v-col>
 
             <!-- scrollable content -->
@@ -455,31 +338,38 @@
 
 <script>
 import Email from './helpers/Email';
+import TableOfContentsSidebar from './navigation/TableOfContentsSidebar';
 
 export default {
     name: 'Project',
-    components: {Email},
+    components: {Email, TableOfContentsSidebar},
+    data() {
+        return {
+            headings: [
+                {
+                    href: '#researchObjectives',
+                    label: 'Forschungsfragen & Projektziele',
+                },
+                {
+                    href: '#teamAndPartners',
+                    label: 'Team & Partner:innen',
+                },
+                {
+                    href: '#presentationsAndPublications',
+                    label: 'Projektpräsentationen & Publikationen',
+                },
+                {
+                    href: '#pressReports',
+                    label: 'Presseberichte',
+                },
+            ],
+        };
+    },
 };
 </script>
 
 <style scoped>
 .hyphens-auto {
     hyphens: auto;
-}
-
-.sticky-top {
-    position: sticky;
-    top: 0;
-}
-
-.toc-border-left {
-    border-color: #0000001f;
-    border-left-style: solid;
-    border-left-width: thin;
-}
-
-/* to enable linebreaks for long labels */
-.white-space-normal {
-    white-space: normal;
 }
 </style>
