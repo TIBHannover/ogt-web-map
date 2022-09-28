dfcea449893c81d5d7399169ddfd532a2523e1e8
Nils Casties <nils.casties@gmail.com>
2022-09-28 13:24:59 +0200

    [OGT-322] Set default background color.

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,7 +1,7 @@
 <template>
     <v-app>
         <navigation-sidebar></navigation-sidebar>
-        <v-main>
+        <v-main class="grey lighten-3">
             <router-view></router-view>
         </v-main>
         <info-footer></info-footer>
