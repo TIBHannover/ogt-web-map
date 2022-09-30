904cef06a6cac6410c8b2e947702ea40f52ca0fb
Nils Casties <nils.casties@gmail.com>
2022-09-30 09:55:25 +0200

    [OGT-322] Updated charts overview template.

--- resources/js/components/Charts.vue
+++ resources/js/components/Charts.vue
@@ -1,21 +1,32 @@
 <template>
-    <v-container
-        class="grey lighten-3 justify-center"
-        fill-height
-        fluid
-    >
-        <div class="text-center">
-            <p class="text-h3 text-sm-h1">Visualisierungen</p>
-        </div>
+    <v-container>
+        <!-- header -->
+        <v-row class="ml-14 ml-sm-n3">
+            <v-col>
+                <h1 class="hyphens-auto py-4 text-center text-h4 text-md-h3" lang="de">
+                    Datenvisualisierung
+                </h1>
+            </v-col>
+        </v-row>
+
     </v-container>
 </template>
 
 <script>
+import chartsData from '../data/chartsData';
+
 export default {
     name: 'Charts',
+    data() {
+        return {
+            charts: chartsData,
+        };
+    },
 };
 </script>
 
 <style scoped>
-
+.hyphens-auto {
+    hyphens: auto;
+}
 </style>
