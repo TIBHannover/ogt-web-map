8d569013d34ae05d8461170ebff6fbaac07fa10e
Nils Casties <nils.casties@gmail.com>
2022-10-06 10:44:29 +0200

    [OGT-322] Added visualization of charts using the Wikidata Query Service.

--- resources/js/components/Charts.vue
+++ resources/js/components/Charts.vue
@@ -13,19 +13,17 @@
         <!-- charts overview -->
         <section v-if="showOverview">
             <v-row>
-                <v-col cols=12 class="hyphens-auto text-justify" lang="de">
-                    <h2 class="text-h6 text-md-h5 mb-3">
+                <v-col class="hyphens-auto text-justify" cols=12 lang="de">
+                    <h2 class="mb-3 text-h6 text-md-h5">
                         Visualisierung von Daten zu Organisationen, Ereignissen, Erinnerungsorten, Tätern und
                         Geschädigten
                     </h2>
                     <p>
                         Die Daten auf dieser Seite werden direkt auf Wikidata abgefragt und so ständig aktualisiert. Je
-                        mehr
-                        Daten vorhanden sind, desto vollständiger werden die Übersichten. Dieses Ziel kann nur durch die
-                        Hilfe vieler erreicht werden. Informationen, wie mitgeforscht werden kann, finde sich hier:
-                        <router-link to="/collaboration">
-                            Mitforschen
-                        </router-link>
+                        mehr Daten vorhanden sind, desto vollständiger werden die Übersichten. Dieses Ziel kann nur
+                        durch die Hilfe vieler erreicht werden. Informationen, wie mitgeforscht werden kann, finde sich
+                        hier:
+                        <router-link to="/collaboration">Mitforschen</router-link>
                     </p>
                 </v-col>
             </v-row>
@@ -35,16 +33,54 @@
                 <v-col class="col-12 col-sm-6" v-for="(chart, chartName) in this.charts" :key="chartName">
                     <v-card>
                         <v-img :src="$ogtGlobals.proxyPath + chart.imageUrl"></v-img>
-                        <v-card-title>{{ chart.title }}</v-card-title>
-                        <v-card-subtitle>{{ chart.subtitle }}</v-card-subtitle>
+                        <v-card-title>
+                            {{ chart.title }}
+                        </v-card-title>
+                        <v-card-subtitle class="hyphens-auto text-justify" lang="de">
+                            {{ chart.subtitle }}
+                        </v-card-subtitle>
                         <v-card-actions>
-                            <v-btn @click.stop="showVisualization(chartName)" outlined rounded>
+                            <v-btn @click.stop="showChart(chart)" outlined rounded>
                                 Anzeigen
                             </v-btn>
                         </v-card-actions>
                     </v-card>
                 </v-col>
             </v-row>
+        </section>
+
+        <!-- chart visualization -->
+        <section v-if="!showOverview">
+            <v-row justify="space-between">
+                <v-col sm="5">
+                    <v-btn @click.stop="showOverview = !showOverview" outlined rounded>
+                        <v-icon left>
+                            mdi-arrow-left
+                        </v-icon>
+                        Zurück zur Übersicht
+                    </v-btn>
+                </v-col>
+                <v-col class="maxWidth100" sm="5">
+                    <v-btn class="float-sm-right" :href="selectedChart.queryUrl" outlined rounded target="_blank">
+                        Im Wikidata Query Service anzeigen
+                        <v-icon right>
+                            mdi-open-in-new
+                        </v-icon>
+                    </v-btn>
+                </v-col>
+            </v-row>
+
+            <h1 class="text-h5 text-md-h4 my-5">
+                {{ selectedChart.title }}
+            </h1>
+            <p class="hyphens-auto text-justify" lang="de">
+                {{ selectedChart.subtitle }}
+            </p>
+
+            <iframe referrerpolicy="origin"
+                    sandbox="allow-scripts allow-same-origin allow-popups"
+                    :src="selectedChart.queryUrl"
+            ></iframe>
         </section>
     </v-container>
 </template>
@@ -57,18 +93,18 @@ export default {
     data() {
         return {
             charts: chartsData,
-            displayedChart: '',
+            selectedChart: null,
             showOverview: true,
         };
     },
     methods: {
         /**
-         * Show visualization and hide charts overview.
+         * Show selected chart and hide charts overview.
          *
-         * @param chartName
+         * @param {Object} chart
          */
-        showVisualization(chartName) {
-            this.displayedChart = chartName;
+        showChart(chart) {
+            this.selectedChart = chart;
             this.showOverview = false;
         },
     },
@@ -83,5 +119,16 @@ export default {
 
 .hyphens-auto {
     hyphens: auto;
+}
+
+iframe {
+    border: none;
+    height: 80vh;
+    width: 100%;
+}
+
+/* workaround to avoid the button not being fully visible on small devices */
+.maxWidth100 {
+    max-width: 100%;
 }
 </style>
