96c7fec636cc060c19f9904e8dcc0b92401f885a
Nils Casties <nils.casties@gmail.com>
2022-10-17 14:18:17 +0200

    [OGT-294] Added template view for victim data in map info sidebar.

---
+++ resources/js/components/map/info/Victim.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <!-- place of detention - https://www.wikidata.org/wiki/Property:P2632 -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Victim',
+    props: ['selectedPerson'],
+};
+</script>
+
+<style scoped>
+
+</style>
